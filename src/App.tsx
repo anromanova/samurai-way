98290cb10dd7f2a89d9bf3c2bc13c31b270b20c1
Anastasia Romanova <anastasiya.erohina@gmail.com>
2023-10-21 22:11:07 +0100

    lesson 18 add Dialogs page

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import {Header} from "./components/Header/Header";
 import {Sidebar} from "./components/Sidebar/Sidebar";
 import {Profile} from "./components/Profile/Profile";
 import {Footer} from "./components/Footer/Footer";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
-    return (
-        <div className="app-wrapper">
-            <Header/>
-            <Sidebar/>
-            <Profile/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="app-wrapper">
+      <Header/>
+      <Sidebar/>
+      <div className="app-wrapper__content">
+        {/*<Profile/>*/}
+        <Dialogs/>
+      </div>
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Dialogs.module.scss';
+const Dialogs = () => {
+  return (
+    <div>
+      Dialogs
+    </div>
+  );
+};
+
+export default Dialogs;
