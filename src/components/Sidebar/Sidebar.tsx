ef0bfdc6e599006af16065ce4ac32c3ae29c508a
Anastasia Romanova <anastasiya.erohina@gmail.com>
2023-10-21 23:08:02 +0100

    lesson 20 update anchor to be Navlink

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import s from './Sidebar.module.scss';
+import {NavLink} from "react-router-dom";
+
 export const Sidebar = () => {
-    return (
-        <nav className={s.sidebar}>
-            <ul className={s.nav}>
-                <li><a href="/profile">Profile</a></li>
-                <li><a href="/dialogs">Messages</a></li>
-                <li><a href="/news">News</a></li>
-                <li><a href="/music">Music</a></li>
-                <li><a href="/settings">Settings</a></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className={s.sidebar}>
+      <ul className={s.nav}>
+        <li><NavLink to="/profile" activeClassName={s.active}>Profile</NavLink></li>
+        <li><NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink></li>
+        <li><NavLink to="/news" activeClassName={s.active}>News</NavLink></li>
+        <li><NavLink to="/music" activeClassName={s.active}>Music</NavLink></li>
+        <li><NavLink to="/settings" activeClassName={s.active}>Settings</NavLink></li>
+      </ul>
+    </nav>
+  );
 };
