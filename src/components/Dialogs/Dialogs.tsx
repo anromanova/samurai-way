341bb619ad5245336c40052088c8034779d4efdf
Anastasia Romanova <anastasiya.erohina@gmail.com>
2023-10-21 23:46:24 +0100

    lesson 21 base dialog page styling

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import './Dialogs.module.scss';
+import s from './Dialogs.module.scss';
 const Dialogs = () => {
   return (
     <div>
-      Dialogs
+      <div className={s.dialogs}>
+        <div className={s.dialogItems}>
+          <div className={`${s.dialog} ${s.active}`}>Nancy</div>
+          <div className={s.dialog}>Max</div>
+          <div className={s.dialog}>Zoe</div>
+          <div className={s.dialog}>Leo</div>
+        </div>
+        <div className={s.messages}>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>How are you</div>
+          <div className={s.message}>Nice to see you</div>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>Hello</div>
+        </div>
+      </div>
     </div>
   );
 };
